28a1c4563246332dc39bd02798fc4ff7c6320196
luann_sapucaia <luann.sapucaia@ingresso.com>
2022-05-04 00:00:53 -0300

    removed unnecessary comments

--- pecas/Peca.js
+++ pecas/Peca.js
@@ -6,12 +6,9 @@
   imagemSrc;
 
   getImagem() {
-    console.log('teste')
     if (this.ehBranco) {
-        console.log('testeBranca')
         this.imagemSrc =  "../images/" + this.tipo + 'l' + ".png";
     } else {
-        console.log('testePreta')
         this.imagemSrc =  "../images/" + this.tipo + 'd' + ".png";
     }
   }
