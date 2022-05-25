1d80046adacd31a6af1239aef11dd37bb5dcffa4
Matheus Veríssimo <matheusrover13@gmail.com>
2022-05-24 21:59:21 -0300

    criada classe de rei (ainda nao funcional)

---
+++ rei.js
@@ -0,0 +1,13 @@
+class Rei extends Peca{
+
+    constructor(linha, coluna, cor){
+        super(linha, coluna, cor, "k")
+    }
+
+    getMovimentosPossiveis(casas){
+        var casasPossiveis = []
+        //mais complexo, pois nao pode andar por casas atacadas wip
+
+        return casasPossiveis
+    }
+}
